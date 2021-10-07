d7ff142fdc56dd64a1f9dcb4f08d920386668e11
deecooper <deecoopermedia@gmail.com>
2021-10-07 22:27:46 +0000

    Add multiplication code

--- assests/js/script.js
+++ assests/js/script.js
@@ -31,7 +31,13 @@ runGame("addition");
 
     if (gameType === "addition") {
         displayAdditionQuestion(num1, num2);
-    } else {
+    } else if (gameType === "multiply") {
+        displayMultiplyQuestion(num1, num2);
+
+
+    }
+    
+    else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
     }
@@ -64,7 +70,10 @@ function calculateCorrectAnswer () {
 
     if (operator === "+") {
         return [operand1 + operand2, "addition"];
-    } else {
+    } else if (operator === "x") {
+        return [operand1 * operand2, "multiply"];
+    }
+    else {
         alert(`Unimplemented operator ${operator}`);
         throw `Ùnimplemented operator ${operator}.Aborting!`
     }
@@ -102,6 +111,9 @@ function displaySubtractQuestion() {
 
 }
 
-function displayMultiplyQuestion() {
+function displayMultiplyQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "x";
 
 }
