0e48c94adecf9680e58cf5d92e6ce7366cb0db69
deecooper <deecoopermedia@gmail.com>
2021-10-07 22:39:41 +0000

    Add code for subraction game

--- assests/js/script.js
+++ assests/js/script.js
@@ -33,8 +33,8 @@ runGame("addition");
         displayAdditionQuestion(num1, num2);
     } else if (gameType === "multiply") {
         displayMultiplyQuestion(num1, num2);
-
-
+    } else if (gameType === "subtract") {
+        displaySubtractQuestion(num1, num2);
     }
     
     else {
@@ -72,6 +72,8 @@ function calculateCorrectAnswer () {
         return [operand1 + operand2, "addition"];
     } else if (operator === "x") {
         return [operand1 * operand2, "multiply"];
+    } else if (operator === "-") {
+        return [operand1 - operand2, "subtract"];
     }
     else {
         alert(`Unimplemented operator ${operator}`);
@@ -107,7 +109,11 @@ function displayAdditionQuestion(operand1, operand2) {
     document.getElementById('operator').textContent = "+";
     
 }
-function displaySubtractQuestion() {
+function displaySubtractQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1 > operand2? operand1 : operand2;
+    document.getElementById('operand2').textContent = operand1 > operand2? operand2 : operand1;
+    document.getElementById('operator').textContent = "-";
+
 
 }
 
